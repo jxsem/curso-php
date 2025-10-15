5e1410d707491bea2c49cd891310b4d9554c315d
Jose Manuel <soldadojimenezj@gmail.com>
2025-10-15 10:34:08 +0200

    Uso de constantes en php

---
+++ constantes.php
@@ -0,0 +1,12 @@
+<?php 
+// constantes -> datos que no cambian
+
+// sintaxis antigua
+define("nombre","Jose"); // -> palabra reservada define("nombre-const", "info-const")
+
+echo nombre;
+
+// sintaxis nueva
+const nombre2 = 12345; // palabra resevada const -> nombre-variable -> info-const
+
+echo nombre2;
