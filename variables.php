fe9a1f0e4aad4f2c26a813e582061bd069bbe49c
Jose Manuel <soldadojimenezj@gmail.com>
2025-10-15 10:33:05 +0200

    Uso de variables en php

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+// ¿Como definir una variable y mostrar el valor?
+$nombre = "Jose"; // se empiezan con el simbolo del dólar
+echo $nombre;
+
+// Pueden empezar tambien con un guion bajo o con la letra en mayuscula
+$_numero = 4;
+echo $_numero;
