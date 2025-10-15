a5fcd84e237ab87f758008f779fa7b7fced0bb1a
Jose Manuel <soldadojimenezj@gmail.com>
2025-10-15 10:33:25 +0200

    Conociendo los tipos de datos en php

---
+++ tipos-de-datos.php
@@ -0,0 +1,8 @@
+<?php
+    // Tipos de datos
+    echo TRUE; // Boolean true -> 1 false -> 0
+    var_dump(TRUE);
+    echo 10;
+    var_dump(-10); // var_dump es para que te diga el tipo de dato
+    var_dump(6.5); //Float
+    var_dump("Hola, PHP");
